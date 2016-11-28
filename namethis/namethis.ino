7811ddbec90a818ca3a12205bbc052773b48ed6f
KarkatVantass <marty.ganev@gmail.com>
2016-11-28 08:48:49 +0200

    Callibration Algo

--- namethis/namethis.ino
+++ namethis/namethis.ino
@@ -1,28 +1,24 @@
+int motor1 = ;
+int motor2 = ;
+int speed1 = ;
+int speed2 = ;
+
+int button = ;
 int sensors[5] = {A0, A1, A2, A3, A4};
-int sensorsCount = 5
-bool is_it_bl[];
-int blackTr;
-int arHelper = 0;
-char directions[];
-char getDirection() {
-  for (int i = 0; i < sensorsCount; i++) {
-    is_it_bl[i] = analogRead(sensors[i]) < blackTr;
-  }
-  if (is_it_bl[0]) {
-    return 'R';
-  }
-  if (is_it_bl[2]) {
-    return 'F';
-  }
-  if (is_it_bl[4]) {
-    return 'L';
-  }
-}
+
+int bl_avrg;
+int wh_avrg;
+int tresshold;
+
+int getAvrg() {}
+
 void setup() {
-  
+  while (true) {
+    if (button == HIGH) {
+      bl_avrg = getAvrg();
+    }
+  }
 }
 
 void loop() {
-  directions[arHelper] = getDirection();
-  atHelper+=1
 }
